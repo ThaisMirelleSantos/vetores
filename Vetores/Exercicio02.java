59ce8d5ba338b59d527505a7d04d6d61f7bd2c9a
ThaisMirelleSantos <147115083+ThaisMirelleSantos@users.noreply.github.com>
2023-10-27 08:13:52 -0300

    exercicio 2, 3 e 4 da lista de vetores

---
+++ Vetores/Exercicio02.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class Exercicio02 {
+	
+	public static void main(String[] args) {
+		Scanner in= new Scanner(System.in);
+		int a[] , b[],c[], i;
+		final int TAM=10;
+		a=new int [TAM] ;
+		b=new int [TAM];
+		c=new int [TAM];
+		for(i=0; i<TAM; i++) { 
+			System.out.println("Digite o "+(i+1)+"valor do vetor A");
+			a[i]= in.nextInt();
+		}
+		for(i=0; i<TAM; i++) { 
+			System.out.println("Digite o "+(i+1)+"valor do vetor B");
+			b[i]= in.nextInt();
+			c[i]= a[i]+b[i];
+		}
+		System.out.print("\nC = ");
+		for(i=0; i<TAM; i++) {
+			System.out.print(c[i]+" ");
+		}
+	}
+}
